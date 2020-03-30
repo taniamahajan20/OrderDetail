7892ac22de66556889da15db40c36362abb209b0
Tania Mahajan <Tania.Mahajan@blackbaud.com>
2020-03-30 11:27:21 +0530

    Add project files.

---
+++ Controllers/OrderdetailController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderDetail.Controllers
+{
+    [Route("orderdetails")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet("{id}")]
+        public ActionResult<Orderdetail> Get(int id)
+        {
+            Orderdetail orderdetail = new Orderdetail();
+            string userapi = (Environment.GetEnvironmentVariable("userapi")!=null ? Environment.GetEnvironmentVariable("userapi"): "https://localhost:1234/");
+            string orderapi = (Environment.GetEnvironmentVariable("orderapi")!=null ? Environment.GetEnvironmentVariable("orderapi"): "https://localhost:2345/");
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(userapi);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = client.GetAsync("user/1").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseString = response.Content.ReadAsStringAsync().Result;
+                    orderdetail.UserDetails = response.Content.ReadAsAsync<User>().Result;
+                }
+            }
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(orderapi);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = client.GetAsync("orders/1").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseString = response.Content.ReadAsStringAsync().Result;
+                    orderdetail.Orders = response.Content.ReadAsAsync<IEnumerable<Order>>().Result;
+                }
+            }
+
+            return Ok(orderdetail);
+        }
+    }
+}
---
+++ Orderdetail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderDetail
+{
+    public class Orderdetail
+    {
+        public User UserDetails { get; set; }
+        public IEnumerable<Order> Orders { get; set; }
+    }
+}
